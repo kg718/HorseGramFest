dbb763f6a6fdce766d251505f198546a2babd0eb
kg718 <kggoodall18@gmail.com>
2025-01-13 17:21:26 +0000

    Commit lasers and Iframes

--- HorseGramFestProject/Assets/Scripts/Laser.cs
+++ HorseGramFestProject/Assets/Scripts/Laser.cs
@@ -1,18 +1,63 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Laser : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField] private Transform generator1;
+    [SerializeField] private Transform generator2;
+
+    [Space]
+    [SerializeField] private bool hasCycles = true;
+    private bool isLaserActive = true;
+    [SerializeField] private float activationTime;
+    [SerializeField] private float activationDelay;
+    private float currentActivationTimer = 0f;
+
+    private LineRenderer line;
+    
     void Start()
     {
-        
+        line = GetComponent<LineRenderer>();
+        line.SetPosition(0, generator1.position);
+        line.SetPosition(1, generator2.position);
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (isLaserActive)
+        {
+            DetectShip();
+            line.enabled = true;
+        }
+        else
+        {
+            line.enabled = false;
+        }
+        if(hasCycles)
+        {
+            currentActivationTimer += Time.deltaTime;
+            if (isLaserActive && currentActivationTimer >= activationTime)
+            {
+                isLaserActive = false;
+                currentActivationTimer = 0f;
+            }
+            else if(!isLaserActive && currentActivationTimer >= activationDelay)
+            {
+                isLaserActive = true;
+                currentActivationTimer = 0f;
+            }
+        }
+    }
+
+    public void DetectShip()
+    {
+        RaycastHit _hit;
+        Vector3 _dir = generator2.transform.position - generator1.transform.position;
+        float _dist = Vector3.Distance(generator1.transform.position, generator2.transform.position);
+        Physics.Raycast(generator1.position, _dir, out _hit, _dist);
+        Debug.DrawRay(generator1.position, _dir, Color.green);
+        if (_hit.collider.gameObject.tag == "Player")
+        {
+            _hit.collider.gameObject.GetComponent<PlayerDamage>().TakeDamage(25);
+        }
     }
 }
--- HorseGramFestProject/Assets/Scripts/PlayerDamage.cs
+++ HorseGramFestProject/Assets/Scripts/PlayerDamage.cs
@@ -5,14 +5,55 @@ public class PlayerDamage : MonoBehaviour
     [SerializeField] private int health;
     private Rigidbody rb;
 
+    [SerializeField] private float invulnarabilityWindow;
+    private float currentInvulnarabilityTimer = 0f;
+    private bool isInvulnarable = false;
+
+    [Space]
+    //[SerializeField, Range(0, 255)] private int flashAlpha;
+    [SerializeField] private Color colourFlash;
+    [SerializeField] private float flashFrequency;
+    private Color defaultColour;
+    private float currentFlashTimer = 0f;
+    private bool isFlashing = false;
+    [SerializeField] private MeshRenderer shipMesh;
+
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        defaultColour = shipMesh.material.color;
     }
 
     void Update()
     {
-
+        if(isInvulnarable)
+        {
+            currentInvulnarabilityTimer += Time.deltaTime;
+            if(currentInvulnarabilityTimer >= invulnarabilityWindow)
+            {
+                isInvulnarable = false;
+                isFlashing = false;
+                shipMesh.material.color = defaultColour;
+                currentInvulnarabilityTimer = 0f;
+                currentFlashTimer = 0f;
+            }
+        }
+        if (isFlashing)
+        {
+            currentFlashTimer += Time.deltaTime;
+        }
+        if (currentFlashTimer > flashFrequency)
+        {
+            currentFlashTimer = 0f;
+            if (shipMesh.material.color == defaultColour)
+            {
+                shipMesh.material.color = colourFlash;
+            }
+            else
+            {
+                shipMesh.material.color = defaultColour;
+            }
+        }
     }
 
     private void OnCollisionEnter(Collision collision)
@@ -30,10 +71,21 @@ public class PlayerDamage : MonoBehaviour
 
     public void TakeDamage(int _damage)
     {
-        health -= _damage;
-        if(health <= 0)
+        if(!isInvulnarable)
         {
-            
+            health -= _damage;
+            isInvulnarable = true;
+            ActivateFlash();
+            if (health <= 0)
+            {
+
+            }
         }
+    }
+
+    public void ActivateFlash()
+    {
+        isFlashing = true;
+        shipMesh.material.color = colourFlash;
     }
 }
