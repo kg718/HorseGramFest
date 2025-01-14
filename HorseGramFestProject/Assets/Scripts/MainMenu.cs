becdf272e2ee41d88dcb83dddf04c73de868e4f7
kg718 <kggoodall18@gmail.com>
2025-01-14 15:08:18 +0000

    Commit Transition

--- HorseGramFestProject/Assets/Scripts/MainMenu.cs
+++ HorseGramFestProject/Assets/Scripts/MainMenu.cs
@@ -3,21 +3,17 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
-
-
-    void Start()
-    {
-        
-    }
-
-    void Update()
-    {
-        
-    }
+    private int index;
 
     public void OnClickStart(int _sceneIndex)
     {
-        SceneManager.LoadSceneAsync(_sceneIndex);
+        index = _sceneIndex;
+        Invoke("loadScene", 1);
+    }
+
+    private void loadScene()
+    {
+        SceneManager.LoadSceneAsync(index);
     }
 
     public void OnClickQuit()
