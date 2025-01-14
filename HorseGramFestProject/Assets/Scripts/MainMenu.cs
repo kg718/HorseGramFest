c99264a2bbd81b7b4f2dcde7185072a2e54844e4
kg718 <kggoodall18@gmail.com>
2025-01-14 15:29:33 +0000

    Commit Menu Transition

--- HorseGramFestProject/Assets/Scripts/MainMenu.cs
+++ HorseGramFestProject/Assets/Scripts/MainMenu.cs
@@ -3,12 +3,14 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    [SerializeField] private Animator transitionAnimator;
     private int index;
 
     public void OnClickStart(int _sceneIndex)
     {
         index = _sceneIndex;
-        Invoke("loadScene", 1);
+        transitionAnimator.Play("TransitionPanel_In");
+        Invoke("loadScene", 1.5f);
     }
 
     private void loadScene()
